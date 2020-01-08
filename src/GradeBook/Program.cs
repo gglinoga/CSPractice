5bb26829925bc472099fa3eaf5243444c452b88c
Greg Glinoga <46800626+gglinoga@users.noreply.github.com>
2020-01-07 17:53:40 -0800

    loops

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,17 +9,28 @@ namespace GradeBook
         {
 
             var book = new Book("Greg's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+
+            while(true)
+            {
+                Console.WriteLine("Enter grades.  Enter q to exit");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+
+            }
+
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The average grade is {stats.Average:N2}");
             Console.WriteLine($"The high grade is {stats.High}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The Letter grade is {stats.Letter}");
 
 
-            
         }
     }
 }
